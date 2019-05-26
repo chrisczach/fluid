bdb11331ebe51332aebd66720f820aa5e78a8381
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-05-25 19:13:21 -0700

    working on schemas

--- studio/schemas/category.js
+++ studio/schemas/category.js
@@ -1,21 +1,30 @@
-import MdApps from "react-icons/lib/md/apps";
+import MdApps from 'react-icons/lib/md/apps'
 
 export default {
-  name: "category",
-  title: "Category",
-  type: "document",
+  name: 'category',
+  title: 'Equipment Category',
+  type: 'document',
   icon: MdApps,
   fields: [
     {
-      name: "title",
-      title: "Title",
-      type: "string"
+      name: 'title',
+      title: 'Title',
+      type: 'string'
     },
     {
-      name: "description",
-      title: "Description",
-      type: "text"
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      description: 'Some frontend will require a slug to be set to be able to show the project',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text'
     }
-  ],
-  liveEdit: true
-};
+  ]
+}
---
+++ studio/schemas/equipment.js
@@ -0,0 +1,55 @@
+export default {
+  name: 'equipment',
+  title: 'Equipment Item',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      description: 'Some frontend will require a slug to be set to be able to show the project',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'blockText'
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'mainImage'
+    },
+    {
+      name: 'categories',
+      title: 'Equipment Categories',
+      type: 'reference',
+      to: { type: 'category' }
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent'
+    }
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      image: 'mainImage'
+    },
+    prepare ({ title = 'No title', image }) {
+      return {
+        title,
+        media: image
+      }
+    }
+  }
+}
