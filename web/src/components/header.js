c6294dc66b4131f25c0a8d4a9f5ac7892f360bd9
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-05-30 23:53:46 -0700

    change nav bar

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -7,6 +7,21 @@ import ReactSVG from 'react-svg'
 import { imageUrlFor } from '../lib/image-url'
 import styles from './header.module.css'
 
-const Header = ({ logo }) => <div className={styles.wrapper} />
+const Header = () => (
+  <div className={styles.wrapper}>
+    <Link className={styles.link} id='about' to='/about'>
+      about
+    </Link>
+    <Link className={styles.link} id='equipment' to='/equipment'>
+      equipment
+    </Link>
+    {/* <Link id="blog" className="menu-item" to="/blog">
+        Blog
+      </Link> */}
+    <Link className={styles.link} id='contact' to='/contact'>
+      contact
+    </Link>
+  </div>
+)
 
 export default Header
