88884924a4c271b9e06a9e9d2dbf8d0b171bd75b
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-08-16 09:07:16 -0700

    will this work?

--- web/src/lib/image-url.js
+++ web/src/lib/image-url.js
@@ -6,3 +6,10 @@ const builder = imageUrlBuilder(sanityConfig.api)
 export function imageUrlFor(source) {
   return builder.image(source)
 }
+
+const herokuConfigAPI = {
+  api: {
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET
+  }
+}
