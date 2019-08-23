8a189ee1a296818b6a41119fcebcb939005d4f1e
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-08-23 14:39:46 -0700

    comment out break from bumping reactsvg

--- web/src/components/burger-menu.js
+++ web/src/components/burger-menu.js
@@ -6,31 +6,33 @@ import ReactSVG from 'react-svg'
 import { buildImageObj } from '../lib/helpers'
 import { imageUrlFor } from '../lib/image-url'
 
-export default function burgerMenu ({ siteTitle, logo }) {
+export default function burgerMenu({ siteTitle, logo }) {
   const [open, setOpen] = useState(false)
   const closeMenu = () => setOpen(false)
   const handleChange = ({ isOpen }) => setOpen(isOpen)
   return (
     <Menu right pageWrapId={'page-wrap'} isOpen={open} onStateChange={handleChange}>
-      <Link id='about' className='menu-item' to='/about' onClick={closeMenu}>
+      <Link id="about" className="menu-item" to="/about" onClick={closeMenu}>
         about
       </Link>
-      <Link id='equipment' className='menu-item' to='/equipment' onClick={closeMenu}>
+      <Link id="equipment" className="menu-item" to="/equipment" onClick={closeMenu}>
         equipment
       </Link>
       {/* <Link id="blog" className="menu-item" to="/blog" onClick={closeMenu}>
         Blog
       </Link> */}
-      <Link id='contact' className='menu-item' to='/contact' onClick={closeMenu}>
+      <Link id="contact" className="menu-item" to="/contact" onClick={closeMenu}>
         contact
       </Link>
+      {/* 
+      Breaking change on ReactSVG update, need to bump back to prev version or leave component out.
       <div className={styles.branding}>
         <ReactSVG
           svgClassName={styles.svgStyle}
           className={styles.svgWrapper}
           src={logo && imageUrlFor(buildImageObj(logo)).url()}
         />
-      </div>
+      </div> */}
     </Menu>
   )
 }
